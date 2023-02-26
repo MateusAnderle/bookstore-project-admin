5f908b23c2d6f33955ced0362d4f4e0570c7708d
Mateus Anderle da Silva <matsanderle@gmail.com>
2023-02-26 17:59:40 -0300

    New api link

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-// export const api = axios.create({
-//   baseURL: 'https://used-bookstore-api.herokuapp.com',
-// })
-
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
-});
+  baseURL: 'https://used-bookstore-api.herokuapp.com',
+})
